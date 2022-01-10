009160045b4060309c51da9a50fa7ae4b5720076
doorteeth <44419445+doorteeth@users.noreply.github.com>
2022-01-11 01:33:09 +0800

    Update learnCuda.h

--- include/learnCuda.h
+++ include/learnCuda.h
@@ -12,7 +12,8 @@
 }
 
 
-#include <time.h>
+#include <sys/time.h>
+#include <iostream>
 
 double cpuSecond()
 {
@@ -22,27 +23,68 @@ double cpuSecond()
 
 }
 
-void initialData(float *ip,int size) {
+void initialData(float *ip, int size)
+{
     // generate different seed for random number
     time_t t;
     srand((unsigned) time(&t));
-    for (int i=0; i<size; i++) {
-        ip[i] = (float)( rand() & 0xFF )/10.0f;
+    for (int i = 0; i < size; i++)
+    {
+        ip[i] = (float) (rand() & 0xFF) / 10.0f;
     }
 }
 
-void checkResult(float *hostRef, float *gpuRef, const int N) {
+void initialInt(int *ip, int size)
+{
+    for (int i=0;i<size;++i)
+    {
+        ip[i]=i;
+    }
+}
+
+void checkResult(float *hostRef, float *gpuRef, const int N)
+{
     double epsilon = 1.0E-8;
     bool match = 1;
-    for (int i=0; i<N; i++) {
-        if (abs(hostRef[i] - gpuRef[i]) > epsilon) {
+    for (int i = 0; i < N; i++)
+    {
+        if (abs(hostRef[i] - gpuRef[i]) > epsilon)
+        {
             match = 0;
             printf("Arrays do not match!\n");
-            printf("host %5.2f gpu %5.2f at current %d\n",hostRef[i],gpuRef[i],i);
+            printf("host %5.2f gpu %5.2f at current %d\n", hostRef[i], gpuRef[i], i);
             break;
         }
     }
     if (match) printf("Arrays match.\n\n");
 }
+
+void printMatrix(int *C, const int nx, const int ny)
+{
+    int *ic = C;
+    printf("\nMatrix: (%d.%d)\n", nx, ny);
+    for (int iy = 0; iy < ny; ++iy)
+    {
+        for (int ix = 0; ix < nx; ++ix)
+        {
+            printf("%3d", ic[ix]);
+        }
+        ic += nx;
+        printf("\n");
+    }
+    printf("\n");
+}
+
+__global__ void printThreadIndex(int *A, const int nx, const int ny)
+{
+    int ix = threadIdx.x + blockIdx.x * blockDim.x;
+    int iy = threadIdx.y + blockIdx.y * blockDim.y;
+
+    unsigned int idx = iy * nx + ix;
+    printf("thread_id (%d, %d) block_id (%d, %d) coordinate (%d, %d) "
+           "global index %2d ival %2d\n", threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y, ix, iy, idx, A[idx]);
+}
+
+
 
 #endif//LEARNCUDA_H
